f6529eb0168b7b15955816168b0bff8ff78d56c9
frank <franciscodanielmena@outlook.es>
2024-11-21 20:05:40 -0600

    Se agrega DbContext Se crean las migraciones Se crea la clase ProductoDAL Se crea la clase ProductoBLL

---
+++ CapaDatos/ProductoDAL.cs
@@ -0,0 +1,44 @@
+﻿using CapaModelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class ProductoDAL
+    {
+
+        // Crear una instancia privada de nuestro DbContext
+        private TiendaDbContext _db;
+
+        // Crear un método que nos ayude a crear y actualizar un producto
+        public int Guardar(Producto producto, int id = 0, bool esActualizacion = false)
+        {
+            int resultado = 0;
+
+            // Inicializamos nuestro contexto
+            _db = new TiendaDbContext();
+
+            if (esActualizacion) // Actualizar un producto
+            {
+                producto.ProductoId = id;
+
+                _db.Entry(producto).State = System.Data.Entity.EntityState.Modified;
+                _db.SaveChanges();
+
+                resultado = producto.ProductoId;
+            }
+            else // Guardar un producto
+            {
+                _db.Productos.Add(producto);
+                _db.SaveChanges();
+
+                resultado = producto.ProductoId;
+            }
+
+            return resultado;
+        }
+    }
+}
---
+++ CapaLogica/ProductoBLL.cs
@@ -0,0 +1,26 @@
+﻿using CapaDatos;
+using CapaModelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaLogica
+{
+    public class ProductoBLL
+    {
+        // Crear una instancia a mi clase ProductoDAL privada
+        private ProductoDAL _productoDAL;
+
+        // Nuestro método para guardar y actualizar desde nuestra capa BLL
+        public int Guardar(Producto producto, int id = 0, bool esActuaizacion = false)
+        {
+            // Inicializar nuestro ProductoDAL
+            _productoDAL = new ProductoDAL();
+
+            // Retornar el valor
+            return _productoDAL.Guardar(producto, id, esActuaizacion);
+        }
+    }
+}
